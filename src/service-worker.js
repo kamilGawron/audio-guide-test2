f1beed148a8334dbe41b90b0a0478b47aa3e6eda
kamil <kgawron955@gmail.com>
2022-02-18 18:21:50 +0100

    feat: :sparkles: working with sw

---
+++ src/service-worker.js
@@ -0,0 +1,23 @@
+var cacheName = 'secondVersion';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
